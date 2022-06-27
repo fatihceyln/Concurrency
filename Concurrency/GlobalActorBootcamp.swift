f60530e42593702537b3f0b7acd5bbbf8f7cf402
Fatih Kilit <fatih@Fatih-MacBook-Air.local>
2022-06-27 22:56:10 +0300

    globalActor

--- Concurrency/GlobalActorBootcamp.swift
+++ Concurrency/GlobalActorBootcamp.swift
@@ -2,38 +2,43 @@
 //  GlobalActorBootcamp.swift
 //  Concurrency
 //
-//  Created by Fatih Kilit on 26.06.2022.
+//  Created by Fatih Kilit on 27.06.2022.
 //
 
 import SwiftUI
 
+@globalActor struct MyFirstGlobalActor {
+    static var shared: MyNewDataManager = MyNewDataManager()
+}
+
 actor MyNewDataManager {
     
     func getDataFromDatabase() -> [String] {
-        return ["One", "Two", "Three"]
+        return ["One", "Two", "Three", "Four"]
     }
 }
 
 class GlobalActorBootcampViewModel: ObservableObject {
     
-    @Published var dataArray: [String] = []
-    let manager: MyNewDataManager = MyNewDataManager()
+    @MainActor @Published var dataArray: [String] = []
+    // let manager: MyNewDataManager = MyNewDataManager()
+    let manager: MyNewDataManager = MyFirstGlobalActor.shared
     
-    func getData() async {
-        dataArray = await manager.getDataFromDatabase()
+    @MyFirstGlobalActor func getData() async {
+        let data = await manager.getDataFromDatabase()
+        
+        await MainActor.run(body: {
+            self.dataArray = data
+        })
     }
 }
 
 struct GlobalActorBootcamp: View {
-    
     @StateObject private var viewModel: GlobalActorBootcampViewModel = GlobalActorBootcampViewModel()
-     
     var body: some View {
         List {
             ForEach(viewModel.dataArray, id: \.self) {
                 Text($0)
-                    .font(.title)
-                    .fontWeight(.black)
             }
         }
         .task {
